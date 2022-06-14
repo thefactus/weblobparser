8afebba9a3fc81c9fbee1bfbbed1098786316df1
Pablo Bello <pablobello@hey.com>
2022-06-14 11:35:41 +0100

    Add IPv4 validation spec

--- spec/web_log_parser/line_parser_spec.rb
+++ spec/web_log_parser/line_parser_spec.rb
@@ -2,14 +2,25 @@
 
 RSpec.describe WebLogParser::LineParser do
   describe '#parse' do
-    context 'when a valid log line is provided' do
-      it 'returns an array with the path and the ip address' do
-        valid_line = '/home 87.74.253.145'
-        line_parser = described_class.new
+    context 'when log line string is provided' do
+      context 'when path and ip address are valid' do
+        it 'returns an array with the path and the ip address' do
+          valid_line = '/home 87.74.253.145'
+          line_parser = described_class.new
 
-        result = line_parser.parse(valid_line)
+          result = line_parser.parse(valid_line)
 
-        expect(result).to eq(['/home', '87.74.253.145'])
+          expect(result).to eq(['/home', '87.74.253.145'])
+        end
+      end
+
+      context 'when the ip address is a invalid ipv4' do
+        it 'raises a InvalidIpv4Error exception' do
+          invalid_ipv4_line = '/home 184.123.665.067'
+          line_parser = described_class.new
+
+          expect { line_parser.parse(invalid_ipv4_line) }.to raise_error(WebLogParser::InvalidIpv4Error)
+        end
       end
     end
   end
