16b067e1b3ac3928009cde1c65b30e0668d5f792
Pablo Bello <pablobello@hey.com>
2022-06-15 03:12:24 +0100

    Update web log spec

--- spec/web_log_parser/web_log_spec.rb
+++ spec/web_log_parser/web_log_spec.rb
@@ -20,6 +20,18 @@ RSpec.describe WebLogParser::WebLog do
          '/help_page/1 80 visits', '/home 78 visits']
       )
     end
+
+    context 'when order is ascending' do
+      it 'returns an array with paths and its numbers of visits' do
+        file_path = 'spec/fixtures/webserver.log'
+        web_log = WebLogParser.web_log_from_file(file_path)
+
+        expect(web_log.paths_with_number_of_visits(order: :ascending)).to eq(
+          ['/home 78 visits', '/help_page/1 80 visits', '/about 81 visits', '/index 82 visits',
+           '/contact 89 visits', '/about/2 90 visits']
+        )
+      end
+    end
   end
 
   describe 'paths_with_number_of_unique_visits' do
